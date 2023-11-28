678185c5a65c856cacd3e0c4825ea5bf30e5ec8d
Luis Valdez <lavn4877@gmail.com>
2023-11-28 01:52:18 -0600

    prueba

--- FrontEnd/src/api/axios.js
+++ FrontEnd/src/api/axios.js
@@ -1,9 +1,9 @@
 import  axios  from "axios";
 
-const URL_BACKEND = import.meta.env.VITE_BASE_URL;
+
 
 const instanceAxios =  axios.create({
-    baseURL: URL_BACKEND , 
+    baseURL: process.env.URL_BACKEND, 
     withCredentials:true,
 });
 
