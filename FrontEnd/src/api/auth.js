482d39ded85da1160f972664d550160fc0596e71
Luis Valdez <lavn4877@gmail.com>
2023-11-28 02:12:55 -0600

    correciones env

--- FrontEnd/src/api/auth.js
+++ FrontEnd/src/api/auth.js
@@ -1,7 +1,13 @@
 import axios from './axios.js';
 
-export const registerRequest = (user) =>  axios.post(`/register`,user);
+export const registerRequest = async (user) =>{
+    const res=await axios.post(`/register`,user);
+    console.log(res.data)
+}  
 
-export const loginRequest = user => axios.post(`/login`,user);
+export const loginRequest =  async (user) => {
+  const res = await  axios.post(`/login`,user);
+    console.log(res.data)
+}
 
 export const verifyToken = ()=>axios.get('/verify-token')
